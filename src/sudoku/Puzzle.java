0a77a96e70cbdf6246eaddc462cd84ecc2fcbd72
Youmeng <youmenghin15@gmail.com>
2021-08-05 00:08:36 -0400

    create a constructor in puzzle.java

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -5,4 +5,12 @@ package sudoku;
  * @version 21.08.04
  */
 public class Puzzle {
+    private int[][] board;          //digits on puzzle board
+    private boolean[][] inputCells; //where on the board inputs are allowed (true = allowed)
+    private final int SIZE = 9;     //size constant used to contruct and work with puzzle
+
+    public Puzzle(){
+        board = new int[SIZE][SIZE];
+        inputCells = new boolean[SIZE][SIZE];
+    }
 }
