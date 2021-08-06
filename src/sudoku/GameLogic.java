03d4fccd12e10349f8044be209dce68056521df8
Youmeng <youmenghin15@gmail.com>
2021-08-06 00:49:35 -0400

    add function checkduplicates()

--- src/sudoku/GameLogic.java
+++ src/sudoku/GameLogic.java
@@ -1,5 +1,8 @@
 package sudoku;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Manages the logical component of the game by validating puzzles and checking for completion
  */
@@ -11,6 +14,11 @@ public class GameLogic {
 //        return false;
 //    }
 
+    /**
+     * Checks whether every row in the puzzle board is valid
+     * @param board An array representing the puzzle grid
+     * @return True if no duplicates found in all rows false otherwise
+     */
     public static boolean checkRows(int[][] board) {
         for(int r = 0; r < board.length; r++) {
             if(checkDuplicates(board[r]))
@@ -18,4 +26,24 @@ public class GameLogic {
         }
         return true;
     }
+
+    /**
+     * Takes in an array of 9 integers and check whether duplicates can be found
+     * @param ints An array of 9 integers
+     * @return True if duplicates are found false otherwise
+     */
+    public static boolean checkDuplicates(int[] ints) {
+        Set set = new HashSet();
+        for (int num : ints) {
+            if (set.contains(num)) {
+                return true;
+            }
+            if (num != 0) {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+
+
 }
