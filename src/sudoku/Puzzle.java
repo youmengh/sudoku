8f5220bd38d6d776d42069940fad0785dace9ab8
Youmeng <youmenghin15@gmail.com>
2021-08-05 23:09:11 -0400

    add function generatepuzzle()

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -1,16 +1,40 @@
 package sudoku;
 
+import java.util.Random;
+
 /**
  * @author Youmeng Hin
- * @version 21.08.04
+ * @version 21.08.05
  */
 public class Puzzle {
     private int[][] board;          //digits on puzzle board
     private boolean[][] inputCells; //where on the board inputs are allowed (true = allowed)
     private final int SIZE = 9;     //size constant used to contruct and work with puzzle
 
+    /**
+     * Constructor
+     */
     public Puzzle(){
-        board = new int[SIZE][SIZE];
+        board = generatePuzzle();
         inputCells = new boolean[SIZE][SIZE];
     }
+
+    /**
+     * Generates a new valid sudoku puzzle represented by a 2D int array
+     * @version 21.08.05 - only generates a grid with random numbers
+     *                   - no puzzle validation
+     * @return A 2D int array representing the puzzle board
+     */
+    private int[][] generatePuzzle() {
+        int[][] newBoard = new int[SIZE][SIZE];
+        for(int x  = 0; x < SIZE; x++) {
+            for(int y  = 0; y < SIZE; y++) {
+                Random rand = new Random();
+                newBoard[x][y] = rand.nextInt(9) + 1;
+            }
+        }
+        return newBoard;
+    }
+
+
 }
