201cc3c69192e79fbeef8454f66f011f3212309f
Youmeng <youmenghin15@gmail.com>
2021-08-06 00:43:47 -0400

    add function checkcompletion()

--- src/sudoku/GameLogic.java
+++ src/sudoku/GameLogic.java
@@ -1,4 +1,13 @@
 package sudoku;
 
+/**
+ * Manages the logical component of the game by validating puzzles and checking for completion
+ */
 public class GameLogic {
+
+//    public static boolean checkCompletion(int[][] board) {
+//        if(checkRows(board) && checkCols(board) && checkSquares(board))
+//            return true;
+//        return false;
+//    }
 }
