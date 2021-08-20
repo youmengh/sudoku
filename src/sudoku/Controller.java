4f83095c2251aa218a77c39299067e913d1d08a5
Youmeng <youmenghin15@gmail.com>
2021-08-20 00:06:57 -0400

    override function initialize()

--- src/sudoku/Controller.java
+++ src/sudoku/Controller.java
@@ -3,9 +3,15 @@ package sudoku;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller implements Initializable {
     public GridPane grid;
 
 
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
 }
