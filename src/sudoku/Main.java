b4e7bad16b18dd57f6b7db02deff2138a81e48ca
Youmeng <youmenghin15@gmail.com>
2021-08-19 23:19:20 -0400

    add title to stage

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -5,16 +5,18 @@ import javafx.stage.Stage;
 
 /**
  * @author Youmeng Hin
- * @version 21.08.04
+ * @version 21.08.19
  */
 public class Main extends Application {
     public static void main(String[] args) {
-
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        stage.setTitle("Sudoku (v21.08.19) by Youmeng Hin");
+        stage.show();
     }
 }
 
