a11f58eefc6d1f802e476b95f7f94861c7ef519a
Youmeng <youmenghin15@gmail.com>
2021-08-20 00:05:50 -0400

    controller.java implements initializable class

--- src/sudoku/Controller.java
+++ src/sudoku/Controller.java
@@ -1,4 +1,11 @@
 package sudoku;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+public class Controller implements Initializable {
+    public GridPane grid;
+
+
+
 }
