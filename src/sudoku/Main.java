244231d27f12ae4a1202862f92823c0349155d87
Youmeng <youmenghin15@gmail.com>
2021-08-19 23:02:58 -0400

    main.java extends application class

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,11 +1,19 @@
 package sudoku;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author Youmeng Hin
  * @version 21.08.04
  */
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        
+
     }
+
 }
+
+//    Puzzle p = new Puzzle();
+//    System.out.println(p);
+//    System.out.println(GameLogic.checkCompletion(p.getPuzzleArray()));
